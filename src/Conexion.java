a320c0a08ba56e674771381e3377c344cb9a0b0c
fern055 <fertovarosuna@gmail.com>
2026-07-27 22:08:09 -0600

    se agregó una clase para conexión en el estacionamiento

---
+++ src/Conexion.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexion {
+    private static String server = "localhost";
+    private static String port = "1433";
+    private static String database = "Vehiculos";
+    private static String user = "sa";
+    private static String password = "SqlServer2026!";
+
+    public static String getCadenaConexion() {
+        return String.format(
+                "jdbc:sqlserver://%s:%s;" + "databaseName=%s;" + "user=%s;" + "password=%s;" + "encrypt=true;" + "trustServerCertificate=true;",
+                server, port, database, user, password);
+    }
+
+    public static Connection getConexion() throws Exception {
+        return DriverManager.getConnection(getCadenaConexion());
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
-  public static void Main(String Args[]) {
-    System.out.println("Sistema de Gestion de Estacionamiento");
-    System.out.println("Incluyendo tarifas");
-  }
+    public static void Main(String Args[]) {
+        System.out.println("Sistema de Gestion de Estacionamiento");
+        System.out.println("Incluyendo tarifas");
+    }
 }
