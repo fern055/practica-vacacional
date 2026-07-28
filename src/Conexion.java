60bb8b8930738cb3a5fb02bfd81c6e7967cf6181
fern055 <fertovarosuna@gmail.com>
2026-07-27 21:30:01 -0600

    se agregó una clase para conexión

---
+++ src/Conexion.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexion {
+    private static String server = "localhost";
+    private static String port = "1433";
+    private static String database = "Alumnos";
+    private static String user = "sa";
+    private static String password = "SqlServer2026!";
+    
+    //en server, puede ser "localhost" o la ip del sql server 
+    //en database, es la base de datos por defecto para pruebas
+
+    public static String getCadenaConexion() {
+        return String.format(
+                "jdbc:sqlserver://%s:%s;" + "databaseName=%s;" + "user=%s;" + "password=%s;" + "encrypt=true;" + "trustServerCertificate=true;",
+                server, port, database, user, password);
+    }
+
+    public static Connection getConexion() throws Exception {
+        return DriverManager.getConnection(getCadenaConexion());
+    }
+}
