ff599ccccb2df3e972e9d1cad8b6947f1b4037c6
fern055 <fertovarosuna@gmail.com>
2026-08-19 08:57:28 -0600

    había agregado la conexión antes pero no se subía así que lo volví a crear + intento 2 de funciones sql

--- src/Paquete.java
+++ src/Paquete.java
@@ -1,4 +1,11 @@
-public abstract class Paquete {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Paquete {
+    private int id;
     private String nombreDestinatario;
     private Double pesoKg;
     private EstrategiaEnvio estrategiaEnvio;
@@ -7,10 +14,64 @@ public abstract class Paquete {
 
     }
 
+    private Paquete(int id, String nombreDestinatario, Double pesoKg) {
+        this.id = id;
+        this.nombreDestinatario = nombreDestinatario;
+        this.pesoKg = pesoKg;
+    }
+
+    public Paquete(String nombreDestinatario, Double pesoKg) {
+        this.nombreDestinatario = nombreDestinatario;
+        this.pesoKg = pesoKg;
+    }
+
     public Paquete(String nombreDestinatario, Double pesoKg, EstrategiaEnvio estrategiaEnvio) {
         this.nombreDestinatario = nombreDestinatario;
         this.pesoKg = pesoKg;
         this.estrategiaEnvio = estrategiaEnvio;
+    }
+
+    public int save() throws Exception{
+        try(Connection con = Conexion.getConexion();
+            PreparedStatement stmt = con.prepareStatement("insert into Paqueteria.exa.Paquetes (nombreDestinario, pesoKg) values (?, ?)")
+        ){
+            stmt.setString(1, this.nombreDestinatario);
+            stmt.setDouble(2, this.pesoKg);
+            return stmt.executeUpdate();
+        }
+    }
+
+    public static List<Paquete> showall() throws Exception{
+        try(Connection con = Conexion.getConexion();
+            PreparedStatement stmt = con.prepareStatement("select * from Paqueteria.exa.Paquetes")
+        ){
+            List<Paquete> paquetes = new ArrayList<>();
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()){
+                paquetes.add(new Paquete(rs.getInt("id"), rs.getString("nombreDestinatario"), rs.getDouble("pesoKg")));
+            }
+            return paquetes;
+        }
+    }
+
+    public int modify() throws Exception{
+        try(Connection con = Conexion.getConexion();
+            PreparedStatement stmt = con.prepareStatement("update Paqueteria.exa.Paquetes set nombreDestinatario = ?, pesoKg = ? where id = ?")
+        ){
+            stmt.setString(1, this.nombreDestinatario);
+            stmt.setDouble(2, this.pesoKg);
+            stmt.setInt(3, this.id);
+            return stmt.executeUpdate();
+        }
+    }
+
+    public static int delete(String nombreDestinatario) throws Exception{
+        try(Connection con = Conexion.getConexion();
+        PreparedStatement stmt = con.prepareStatement("delete from Paqueteria.exa.Paquetes where nombreDestinatario = ?")
+        ){
+            stmt.setString(1, nombreDestinatario);
+            return stmt.executeUpdate();
+        }
     }
 
     public Double obtenerCostoEnvio(){
