959de930dd68e6efbe40d8155890c7c8d5094f87
fern055 <fertovarosuna@gmail.com>
2026-08-19 11:24:50 -0600

    mismo error de que no se subió la conexión. intento 3

--- src/CuentaUsuario.java
+++ src/CuentaUsuario.java
@@ -1,14 +1,72 @@
-public abstract class CuentaUsuario {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
+public class CuentaUsuario {
+    private int id;
     private String correoElectronico;
     private int mesesActivo;
     private PlanSuscripcion plan;
 
+    private CuentaUsuario(int id, String correoElectronico, int mesesActivo) {
+        this.id = id;
+        this.correoElectronico = correoElectronico;
+        this.mesesActivo = mesesActivo;
+    }
+
+    public CuentaUsuario(String correoElectronico, int mesesActivo) {
+        this.correoElectronico = correoElectronico;
+        this.mesesActivo = mesesActivo;
+    }
 
     public CuentaUsuario(String correoElectronico, int mesesActivo, PlanSuscripcion plan) {
         this.correoElectronico = correoElectronico;
         this.mesesActivo = mesesActivo;
         this.plan = plan;
+    }
+
+    public int save() throws Exception{
+        try(Connection con = Conexion.getConexion();
+            PreparedStatement stmt = con.prepareStatement("insert into Streaming.exa.Cuentas (correoElectronico, mesesActivos) values (?, ?)")
+        ){
+            stmt.setString(1, this.correoElectronico);
+            stmt.setInt(2, this.mesesActivo);
+            return stmt.executeUpdate();
+        }
+    }
+
+    public static List<CuentaUsuario> showall() throws Exception{
+        try(Connection con = Conexion.getConexion();
+            PreparedStatement stmt = con.prepareStatement("select * from Streaming.exa.Cuentas")
+        ){
+            List<CuentaUsuario> cuentas = new ArrayList<>();
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()){
+                cuentas.add(new CuentaUsuario(rs.getInt("id"), rs.getString("correoElectronico"), rs.getInt("mesesActivo")));
+            }
+            return cuentas;
+        }
+    }
+
+    public int update(String correoElectronico) throws Exception{
+        try(Connection con = Conexion.getConexion();
+            PreparedStatement stmt = con.prepareStatement("update Streaming.exa.Cuentas set correoEñectronico = ?, mesesActivo = ? where correoElectronico = ?")
+        ){
+            stmt.setString(1, this.correoElectronico);
+            stmt.setInt(2, this.mesesActivo);
+            return stmt.executeUpdate();
+        }
+    }
+
+    public static int delete(String correoElectronico) throws Exception{
+        try(Connection con = Conexion.getConexion();
+            PreparedStatement stmt = con.prepareStatement("delete from Streaming.exa.Cuentas where correoElectronico = ?")
+        ){
+            stmt.setString(1, correoElectronico);
+            return stmt.executeUpdate();
+        }
     }
 
     public Double obtenerTotalAPagar(){
