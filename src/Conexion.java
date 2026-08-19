1ed347cd5bf12becde371948200e54f195e54c2b
fern055 <fertovarosuna@gmail.com>
2026-08-19 08:11:52 -0600

    intento

--- src/Conexion.java
+++ src/Conexion.java
@@ -4,7 +4,7 @@ import java.sql.DriverManager;
 public class Conexion {
     private static String server = "localhost";
     private static String port = "1433";
-    private static String database = "Vehiculos";
+    private static String database = "Estacionamiento";
     private static String user = "sa";
     private static String password = "SqlServer2026!";
 
--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -1,4 +1,11 @@
-public abstract class Vehiculo {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vehiculo {
+    private int id;
     private String placa;
     private int horas;
     private Tarifa tarifas;
@@ -7,10 +14,61 @@ public abstract class Vehiculo {
 
     }
 
+    private Vehiculo(int id, String placa, int horas, Tarifa tarifas) {
+        this.id = id;
+        this.placa = placa;
+        this.horas = horas;
+        this.tarifas = tarifas;
+    }
+
     public Vehiculo(String placa, int horas, Tarifa tarifas) {
         this.placa = placa;
         this.horas = horas;
         this.tarifas = tarifas;
+    }
+
+    public int save() throws Exception{
+        try(Connection con = Conexion.getConexion();
+            PreparedStatement stmt = con.prepareStatement("insert into Estacionamiento.exa.Vehiculos (placa, horas, tarifas) values (?, ?, ?)");
+        ){
+            stmt.setString(1, this.placa);
+            stmt.setInt(2, this.horas);
+            return stmt.executeUpdate();
+        }
+    }
+
+    //como le hago con tarifas?
+
+    public static List<Vehiculo> showall() throws Exception{
+        try(Connection con = Conexion.getConexion();
+            PreparedStatement stmt = con.prepareStatement("select * from Estacionamiento.exa.Vehiculos")
+        ){
+            List<Vehiculo> vehiculos = new ArrayList<>();
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()){
+                vehiculos.add(new Vehiculo(rs.getInt("id"), rs.getString("placa"), rs.getInt("horas")))
+            }
+            return vehiculos;
+        }
+    }
+
+    public static int delete(String placa) throws Exception{
+        try(Connection con = Conexion.getConexion();
+            PreparedStatement stmt = con.prepareStatement("delete from Estacionamiento.exa.Vehiculos where placa = ?")
+        ){
+            stmt.setString(1, placa);
+            return stmt.executeUpdate();
+        }
+    }
+
+    public int update(String placa) throws Exception{
+        try(Connection con = Conexion.getConexion();
+            PreparedStatement stmt = con.prepareStatement("update Estacionamiento.exa.Vehiculo set horas = ? where placa = ? ")
+        ){
+            stmt.setInt(1, this.horas);
+            stmt.setInt(2, this.id);
+            return stmt.executeUpdate();
+        }
     }
 
     public String getPlaca() {
